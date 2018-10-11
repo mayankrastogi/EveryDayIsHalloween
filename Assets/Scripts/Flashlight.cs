ba994d44235c203eb741e153e2d48a16609f7bf9
shreyas111 <shreyaskulkarnilogin@gmail.com>
2018-10-11 02:02:07 -0500

    Added Scenes with walls and Prefabs for Walls
    Added Bedroom, Bathroom, Kitchen, Hall, Corridor and Stairway scene.

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Flashlight : VRTK_InteractableObject {
+
+    public Renderer FlashlightRenderer;
+    public Light FlashlightLight;
+    public AudioSource ToggleSound;
+
+    public bool IsFlashlightOn = false;
+
+    private Material FlashlightMaterial;
+
+	// Use this for initialization
+	void Start () {
+        if (FlashlightRenderer != null)
+            FlashlightMaterial = FlashlightRenderer.material;
+
+        if(FlashlightLight == null)
+            FlashlightLight = GetComponentInChildren<Light>();
+
+        if (ToggleSound == null)
+            ToggleSound = GetComponentInChildren<AudioSource>();
+
+        if (IsFlashlightOn)
+            SwitchOn(false);
+        else
+            SwitchOff(false);
+	}
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null) {
+        base.StartUsing(currentUsingObject);
+        SwitchOn();
+    }
+
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true) {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        SwitchOff();
+    }
+
+    public void SwitchOn(bool PlaySoundEffect = true) {
+        StartCoroutine(SwitchOnLights());
+
+        if (PlaySoundEffect && ToggleSound != null)
+            ToggleSound.Play();
+
+        IsFlashlightOn = true;
+    }
+
+    public void SwitchOff(bool PlaySoundEffect = true) {
+        StartCoroutine(SwitchOffLights());
+
+        if (PlaySoundEffect && ToggleSound != null)
+            ToggleSound.Play();
+
+        IsFlashlightOn = false;
+    }
+
+    private IEnumerator SwitchOnLights() {
+        yield return new WaitForSeconds(0.2f);
+
+        if (FlashlightMaterial != null)
+            FlashlightMaterial.EnableKeyword("_EMISSION");
+
+        if (FlashlightLight != null)
+            FlashlightLight.gameObject.SetActive(true);
+    }
+
+    private IEnumerator SwitchOffLights() {
+        yield return new WaitForSeconds(0.2f);
+
+        if (FlashlightMaterial != null)
+            FlashlightMaterial.DisableKeyword("_EMISSION");
+
+        if (FlashlightLight != null)
+            FlashlightLight.gameObject.SetActive(false);
+    }
+}
