c030935c86f97719f6e595d99f9888ed0adb7ea1
shreyas111 <shreyaskulkarnilogin@gmail.com>
2018-10-16 03:44:23 -0500

    Added Zombie Attack Animation

---
+++ Assets/Scripts/ZombieAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Hello()
+    {
+        Debug.Log("Hello");
+    }
+}
